5bb199904493c03afc40faae3a698a3dd63b327e
NV22182 <160574690+NV22182@users.noreply.github.com>
2024-03-05 07:41:39 +0000

    3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
-import { images } from "../constants/index.js";
+import React from 'react';
+import { images } from "../constants";
 
 const Header = () => {
   return (
     <section>
-    <header>
+    <header className="container mx-auto px-5 flex justify-between py-4">
      <div>
-     <img src={images.Logo} alt=""/>
+     <img src={images.Logo} alt="logo" />
      </div>
-     <div></div>
+     <div className='flex gap-x-9'>
+      <ul className='flex gap-x-5'>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/">Articles</a>
+        </li>
+        <li>
+          <a href="/">Pages</a>
+        </li>
+        <li>
+          <a href="/">Pricing</a>
+        </li>
+        <li>
+          <a href="/">Faq</a>
+        </li>
+      </ul>
+      <button>Sign in</button>
+     </div>
     </header>
   </section>
   ) 
