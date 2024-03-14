d07982b64c166f3bf6fc858f060ec553bbdaeef0
NV22182 <160574690+NV22182@users.noreply.github.com>
2024-03-14 08:35:12 +0000

    6-15:19

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -6,9 +6,27 @@ const ArticleCard = ({className}) => {
   return (
     <div className={`rounded-xl overflow-hidden shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px] `}
     >
-        <img src={images.PostImage} alt='title' className='w-full object-cover object-center h-auto' />
+        <img 
+        src={images.PostImage} 
+        alt='title' 
+        className='w-full object-cover object-center h-auto' 
+        />
         <div className='p-5'>
-            <h2 className='font-roboto font-bold text-xl text-dark-soft '>Future of Work</h2>
+            <h2 className='font-roboto font-bold text-xl text-dark-soft '>
+              Future of Work
+              </h2>
+              <p className='text-dark-light mt-3 text-sm'>Majority of people will work in jobs that don't exist today</p>
+              <div className='flex justify-between flex-nowrap items-center mt-6'>
+                <div className='flex items-center gap-x-2'>
+                  <img src={images.PostProfileImage} alt="post profile" />
+                  <div className='flex flex-col'>
+                      <h4 className='font-bold italic text-dark-soft text-sm'>
+                        Viola Mansia
+                      </h4>
+                  </div>
+                </div>
+                <span>02 May</span>
+              </div>
         </div>
     </div>
   )
--- src/constants/images.js
+++ src/constants/images.js
@@ -1,10 +1,12 @@
 import Logo from "../assets/Logo.svg";
 import HeroImage from "/workspaces/blog-project/src/assets/HeroImage.svg";
 import PostImage from "../assets/posts/posts.jpg"
+import PostProfileImage from "../assets/posts/posts-profile (1).svg";
 const images = {
     Logo,
     HeroImage,
     PostImage,
+    PostProfileImage,
 };
 
 export default images;
